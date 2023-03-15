2d718ae7402b17f73e95840b61d960974485230c
Akiba <akiba@anzu.link>
2023-03-16 00:06:56 +0800

    feat: 一堆feat

--- webui.py
+++ webui.py
@@ -1,3 +1,7 @@
+import json
+import os
+import time
+
 import gradio as gr
 from transformers import AutoModel, AutoTokenizer
 from options import parser
@@ -8,6 +12,15 @@ cmd_opts = parser.parse_args()
 
 tokenizer = AutoTokenizer.from_pretrained(cmd_opts.model_path, trust_remote_code=True)
 model = AutoModel.from_pretrained(cmd_opts.model_path, trust_remote_code=True)
+
+_css = """
+#del-btn {
+    max-width: 2.5em;
+    min-width: 2.5em !important;
+    height: 2.5em;
+    margin: 1.5em 0;
+}
+"""
 
 
 def prepare_model():
@@ -55,6 +68,31 @@ def predict(query, max_length, top_p, temperature):
     return readable_history
 
 
+def save_history():
+    if not os.path.exists("outputs"):
+        os.mkdir("outputs")
+
+    s = [{"q": i[0], "o": i[1]} for i in history]
+    filename = f"save-{int(time.time())}.json"
+    with open(os.path.join("outputs", filename), "w", encoding="utf-8") as f:
+        f.write(json.dumps(s, ensure_ascii=False))
+
+
+def load_history(file):
+    global history, readable_history
+    try:
+        with open(file.name, "r", encoding='utf-8') as f:
+            j = json.load(f)
+            _hist = [(i["q"], i["o"]) for i in j]
+            _readable_hist = [(i["q"], parse_codeblock(i["o"])) for i in j]
+    except Exception as e:
+        print(e)
+        return readable_history
+    history = _hist.copy()
+    readable_history = _readable_hist.copy()
+    return readable_history
+
+
 def clear_history():
     history.clear()
     readable_history.clear()
@@ -62,36 +100,58 @@ def clear_history():
 
 
 def create_ui():
-    with gr.Blocks() as demo:
+    with gr.Blocks(css=_css) as demo:
         prompt = "输入你的内容..."
-        gr.Markdown("""<h2><center>ChatGLM WebUI</center></h2>""")
         with gr.Row():
-            max_length = gr.Slider(minimum=4, maximum=4096, step=4, label='Max Length', value=2048)
-            top_p = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Top P', value=0.7)
-            temperature = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Temperature', value=0.95)
+            with gr.Column(scale=3):
+                gr.Markdown("""<h2><center>ChatGLM WebUI</center></h2>""")
+                with gr.Row():
+                    with gr.Column(variant="panel"):
+                        with gr.Row():
+                            max_length = gr.Slider(minimum=4, maximum=4096, step=4, label='Max Length', value=2048)
+                            top_p = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Top P', value=0.7)
+                        with gr.Row():
+                            temperature = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Temperature', value=0.95)
 
-        chatbot = gr.Chatbot(elem_id="chat-box", show_label=False).style(height=800)
-        message = gr.Textbox(placeholder=prompt, show_label=False)
+                        # with gr.Row():
+                        #     max_rounds = gr.Slider(minimum=1, maximum=50, step=1, label="最大对话轮数（调小可以显著改善爆显存，但是会丢失上下文）", value=20)
 
-        with gr.Row():
-            submit = gr.Button("发送")
-            clear_input = gr.Button("清空文本框")
+                with gr.Row():
+                    with gr.Column(variant="panel"):
+                        with gr.Row():
+                            clear = gr.Button("清空对话（上下文）")
 
-        with gr.Row():
-            clear = gr.Button("清空对话（上下文）")
+                        with gr.Row():
+                            save_his_btn = gr.Button("保存对话")
+                            load_his_btn = gr.UploadButton("读取对话", file_types=['file'], file_count='single')
 
-        submit.click(predict,
-                     inputs=[
-                         message,
-                         max_length,
-                         top_p,
-                         temperature
-                     ],
-                     outputs=[
-                         chatbot
-                     ])
+            with gr.Column(scale=7):
+                chatbot = gr.Chatbot(elem_id="chat-box", show_label=False).style(height=800)
+                with gr.Row():
+                    message = gr.Textbox(placeholder=prompt, show_label=False, lines=2)
+                    clear_input = gr.Button("🗑️", elem_id="del-btn")
+
+                with gr.Row():
+                    submit = gr.Button("发送")
+
+        submit.click(predict, inputs=[
+            message,
+            max_length,
+            top_p,
+            temperature
+        ], outputs=[
+            chatbot
+        ])
+
         clear.click(clear_history, outputs=[chatbot])
         clear_input.click(lambda x: "", inputs=[message], outputs=[message])
+
+        save_his_btn.click(save_history)
+        load_his_btn.upload(load_history, inputs=[
+            load_his_btn,
+        ], outputs=[
+            chatbot
+        ])
 
     return demo
 
