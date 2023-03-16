bf15d5bacd15c56d223e7bfe79b0679eda2df05a
Akiba <akiba@anzu.link>
2023-03-16 23:48:59 +0800

    refactor

---
+++ modules/context.py
@@ -0,0 +1,61 @@
+from typing import Optional, List, Tuple
+import json
+import os
+import time
+
+
+def parse_codeblock(text):
+    lines = text.split("\n")
+    for i, line in enumerate(lines):
+        if "```" in line:
+            if line != "```":
+                lines[i] = f'<pre><code class="{lines[i][3:]}">'
+            else:
+                lines[i] = '</code></pre>'
+        else:
+            if i > 0:
+                lines[i] = "<br/>" + line.replace("<", "&lt;").replace(">", "&gt;")
+    return "".join(lines)
+
+
+class Context:
+    def __init__(self, history: Optional[List[Tuple[str, str]]] = None):
+        if history:
+            self.history = history
+        else:
+            self.history = []
+        self.rh = []
+
+    def append(self, query, output) -> str:
+        # c: List[Tuple[str, str]]
+        ok = parse_codeblock(output)
+        self.history.append((query, output))
+        self.rh.append((query, ok))
+        return ok
+
+    def clear(self):
+        self.history = []
+        self.rh = []
+
+    def save_history(self):
+        if not os.path.exists("outputs"):
+            os.mkdir("outputs")
+
+        s = [{"q": i[0], "o": i[1]} for i in self.history]
+        filename = f"save-{int(time.time())}.json"
+        with open(os.path.join("outputs", filename), "w", encoding="utf-8") as f:
+            f.write(json.dumps(s, ensure_ascii=False))
+
+    def load_history(self, file):
+        try:
+            with open(file.name, "r", encoding='utf-8') as f:
+                j = json.load(f)
+                _hist = [(i["q"], i["o"]) for i in j]
+                _readable_hist = [(i["q"], parse_codeblock(i["o"])) for i in j]
+        except Exception as e:
+            print(e)
+        self.history = _hist.copy()
+        self.rh = _readable_hist.copy()
+
+
+ctx = Context()
---
+++ modules/ui.py
@@ -0,0 +1,120 @@
+import os
+
+import gradio as gr
+
+from modules.context import ctx
+from modules.device import torch_gc
+from modules.model import infer
+
+css = "style.css"
+script_path = "scripts"
+_gradio_template_response_orig = gr.routes.templates.TemplateResponse
+
+
+def predict(query, max_length, top_p, temperature):
+    output, history = infer(
+        query=query,
+        history=ctx.history,
+        max_length=max_length,
+        top_p=top_p,
+        temperature=temperature
+    )
+    ok = ctx.append(query, history)
+    print(output)
+    torch_gc()
+    return ok
+
+
+def clear_history():
+    ctx.clear()
+    return gr.update(value=[])
+
+
+def create_ui():
+    with gr.Blocks(css=css, analytics_enabled=False, title="ChatGLM") as demo:
+        with gr.Blocks() as chat_interface:
+            prompt = "输入你的内容..."
+            with gr.Row():
+                with gr.Column(scale=3):
+                    gr.Markdown("""<h2><center>ChatGLM WebUI</center></h2>""")
+                    with gr.Row():
+                        with gr.Column(variant="panel"):
+                            with gr.Row():
+                                max_length = gr.Slider(minimum=4, maximum=4096, step=4, label='Max Length', value=2048)
+                                top_p = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Top P', value=0.7)
+                            with gr.Row():
+                                temperature = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Temperature', value=0.95)
+
+                            # with gr.Row():
+                            #     max_rounds = gr.Slider(minimum=1, maximum=50, step=1, label="最大对话轮数（调小可以显著改善爆显存，但是会丢失上下文）", value=20)
+
+                    with gr.Row():
+                        with gr.Column(variant="panel"):
+                            with gr.Row():
+                                clear = gr.Button("清空对话（上下文）")
+
+                            with gr.Row():
+                                save_his_btn = gr.Button("保存对话")
+                                load_his_btn = gr.UploadButton("读取对话", file_types=['file'], file_count='single')
+
+                with gr.Column(scale=7):
+                    chatbot = gr.Chatbot(elem_id="chat-box", show_label=False).style(height=800)
+                    with gr.Row():
+                        message = gr.Textbox(placeholder=prompt, show_label=False, lines=2)
+                        clear_input = gr.Button("🗑️", elem_id="del-btn")
+
+                    with gr.Row():
+                        submit = gr.Button("发送", elem_id="c_generate")
+
+            submit.click(predict, inputs=[
+                message,
+                max_length,
+                top_p,
+                temperature
+            ], outputs=[
+                chatbot
+            ])
+
+            clear.click(clear_history, outputs=[chatbot])
+            clear_input.click(lambda x: "", inputs=[message], outputs=[message])
+
+            save_his_btn.click(ctx.save_history)
+            load_his_btn.upload(ctx.load_history, inputs=[
+                load_his_btn,
+            ], outputs=[
+                chatbot
+            ])
+
+        interfaces = [
+            (chat_interface, "Chat", "chat"),
+        ]
+
+        with gr.Tabs(elem_id="tabs") as tabs:
+            for interface, label, ifid in interfaces:
+                with gr.TabItem(label, id=ifid, elem_id="tab_" + ifid):
+                    interface.render()
+
+    return demo
+
+
+def reload_javascript():
+    scripts_list = [os.path.join(script_path, i) for i in os.listdir(script_path) if i.endswith(".js")]
+    with open("script.js", "r", encoding="utf8") as js_file:
+        javascript = f'<script>{js_file.read()}</script>'
+
+    for path in scripts_list:
+        with open(path, "r", encoding="utf8") as js_file:
+            javascript += f"\n<script>{js_file.read()}</script>"
+
+    # todo: theme
+    # if cmd_opts.theme is not None:
+    #     javascript += f"\n<script>set_theme('{cmd_opts.theme}');</script>\n"
+
+    def template_response(*args, **kwargs):
+        res = _gradio_template_response_orig(*args, **kwargs)
+        res.body = res.body.replace(
+            b'</head>', f'{javascript}</head>'.encode("utf8"))
+        res.init_headers()
+        return res
+
+    gr.routes.templates.TemplateResponse = template_response
