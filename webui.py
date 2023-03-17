6a80209369bdd5a92fd0deeaf84f58106566120b
Akiba <akiba@anzu.link>
2023-03-17 10:53:55 +0800

    fix bug

--- webui.py
+++ webui.py
@@ -1,8 +1,10 @@
+from modules.model import load_model
 from modules.options import cmd_opts
 from modules.ui import create_ui
 
 
 def main():
+    load_model()
     ui = create_ui()
     ui.queue().launch(
         server_name="0.0.0.0" if cmd_opts.listen else None,
