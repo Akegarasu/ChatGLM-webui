a0d1b174b1ad63c00ac31885128b1e19985fdf3b
be5invis <belleve@typeof.net>
2023-03-19 13:58:20 -0700

    Add a one-click setup and start script for Windows users

---
+++ webui-start.cmd
@@ -0,0 +1,21 @@
+@echo off
+cd /D "%~dp0"
+if exist .venv goto :start
+
+echo "Setup VENV"
+python -m venv .venv
+call .venv\Scripts\activate.bat
+
+echo "Install dependencies"
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install --upgrade -r requirements.txt
+goto :run
+
+:start
+echo "Start VENV"
+call .venv\Scripts\activate.bat
+goto :run
+
+:run
+echo "Start WebUI"
+python webui.py
