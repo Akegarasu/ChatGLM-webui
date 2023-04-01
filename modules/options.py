1e2253b2f3534f39a41c326a7d8e640249cefc2f
FrankZxShen <77840381+FrankZxShen@users.noreply.github.com>
2023-04-01 23:07:42 +0800

    Update options.py

--- modules/options.py
+++ modules/options.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--port", type=int, default="17860")
-parser.add_argument("--model-path", type=str, default="THUDM/chatglm-6b")
+parser.add_argument("--model-path", default="ChatGLM-6B")
 parser.add_argument("--precision", type=str, help="evaluate at this precision", choices=["fp32", "fp16", "int4", "int8"])
 parser.add_argument("--listen", action='store_true', help="launch gradio with 0.0.0.0 as server name, allowing to respond to network requests")
 parser.add_argument("--cpu", action='store_true', help="use cpu")
