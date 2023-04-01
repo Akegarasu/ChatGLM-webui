eff2728568ea3dc43ce77fd1d0a98b2e28758ab0
tangdou1 <35254744+tangdou1@users.noreply.github.com>
2023-04-01 17:37:08 +0800

    改变默认下载地址 提高国内下载速度

--- webui-start.cmd
+++ webui-start.cmd
@@ -7,8 +7,8 @@ python -m venv .venv
 call .venv\Scripts\activate.bat
 
 echo "Install dependencies"
-pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
-pip install --upgrade -r requirements.txt
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 goto :run
 
 :start
